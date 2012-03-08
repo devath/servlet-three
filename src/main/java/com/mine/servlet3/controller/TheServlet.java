9ff7dfbab3f5bc00959a0f978b2ebed6280b2646
fmo <fathmoummou@yahoo.fr>
2012-03-08 14:34:12 +0100

    [servlet-three]Commit #1

---
+++ src/main/java/com/mine/servlet3/controller/TheServlet.java
@@ -0,0 +1,22 @@
+package com.mine.servlet3.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="mytest",
+		urlPatterns={"/url"})		
+public class TheServlet extends HttpServlet {
+    
+	private static final long serialVersionUID = 1L;	
+	
+	@Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+           String jspPath = "view/main.jsp";
+           request.getRequestDispatcher(jspPath).forward(request, response);
+    }
+}
